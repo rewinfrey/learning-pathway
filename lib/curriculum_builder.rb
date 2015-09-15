8b852c49c8d99146a90132ed6691549c2c024b69
Rick Winfrey <rick.winfrey@gmail.com>
2015-09-14 20:26:47 -0500

    Sets up final acceptance test

--- lib/curriculum_builder.rb
+++ lib/curriculum_builder.rb
@@ -1,2 +1,14 @@
+require 'csv'
+
 class CurriculumBuilder
+  attr_reader :domain_order_file_path, :student_tests_file_path
+
+  def initialize(domain_order_file_path, student_tests_file_path)
+    @domain_order_file_path = domain_order_file_path
+    @studnet_tests_file_path = student_tests_file_path
+  end
+
+  def plan
+    []
+  end
 end
--- spec/curriculum_builder_spec.rb
+++ spec/curriculum_builder_spec.rb
@@ -1,4 +1,14 @@
 require 'curriculum_builder'
 
 describe CurriculumBuilder do
+  let(:given_solution)          { CSV.read(File.expand_path('../fixtures/sample_solution.csv', __FILE__)) }
+  let(:domain_order_file_path)  { File.expand_path('../fixtures/domain_order.csv', __FILE__) }
+  let(:student_tests_file_path) { File.expand_path('../fixtures/student_tests.csv', __FILE__) }
+
+  subject { described_class.new(domain_order_file_path, student_tests_file_path) }
+
+  xit "builds a satisfactory curriculum plan" do
+    solution = subject.plan
+    expect(solution).to eq(given_solution)
+  end
 end
